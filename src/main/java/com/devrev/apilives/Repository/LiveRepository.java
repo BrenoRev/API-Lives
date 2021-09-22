793a88347b578f86068a920128a57742285fc871
BrenoRev <silvabreno462@gmail.com>
2021-09-21 22:12:07 -0300

    Criando a classe Repository

--- src/main/java/com/devrev/apilives/Repository/LiveRepository.java
+++ src/main/java/com/devrev/apilives/Repository/LiveRepository.java
@@ -1,5 +1,9 @@
 package com.devrev.apilives.Repository;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +12,7 @@ import com.devrev.apilives.entity.Live;
 @Repository
 public interface LiveRepository extends JpaRepository<Live, Long>{
 
+	Page<Live> findByLiveDateAfterOrderByLiveDateAsc(LocalDateTime date, Pageable pageable);
+    Page<Live> findByLiveDateBeforeOrderByLiveDateDesc(LocalDateTime date, Pageable pageable);
+    
 }
